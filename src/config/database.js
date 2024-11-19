9399a367a02e9a8eb4a1590b16ee58a7417a3af9
sithinguyen <nguyensithi07@gmail.com>
2024-11-19 09:49:59 +0700

    Update evn and server file

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
 // config/database.js
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('fitnessdb', 'root', 'Trunghieu2003', {
-  host: '127.0.0.1',  // hoặc 'localhost'
+const sequelize = new Sequelize(process.env.DATABASE, process.env.USER_DB, process.env.PASSWORD_DB, {
+  host: process.env.HOST_NAME_DB,  // hoặc 'localhost'
   dialect: 'mysql',   // có thể là 'mysql', 'postgres', 'sqlite', hoặc 'mssql'
 });
 
